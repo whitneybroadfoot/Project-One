7b37b063e935323b0bee97e729969edbb828901f
njohnbritto <nirmaljulietneena.johnbritto@charter.com>
2019-12-04 21:20:21 -0700

    updated dropdown image functionality

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ $.ajax({
     method: "GET"
 })
 .then(function(response) {
-   // console.log(response);
+   
     for(var i = 0; i < 6; i++){
         //create a tile with the image from each article and the article title and display on the page
         var article = $('<div>');
         article.attr("class", "column is-one-third");
 
         var urlLink = $('<a>');
-        //console.log("Article URL: " + response.articles[i].url);
+        console.log("Article URL: " + response.articles[i].url);
         var articleURL = response.articles[i].url;
         urlLink.attr("href", articleURL);
 
 
         var articleImage = $('<img>');
-        //console.log("Article Image URL: " + response.articles[i].urlToImage);
+        console.log("Article Image URL: " + response.articles[i].urlToImage);
         var articleImageURL = response.articles[i].urlToImage;
         articleImage.attr("src", articleImageURL);
         articleImage.attr("style", "width: 200px");
@@ -27,7 +27,7 @@ $.ajax({
         article.append(urlLink);
 
         var titleBox = $('<p>');
-        //console.log("Article Title: " + response.articles[i].title);
+        console.log("Article Title: " + response.articles[i].title);
         var articleTitle = response.articles[i].title;
         titleBox.text(articleTitle);
         article.append(titleBox);
@@ -43,36 +43,18 @@ $.ajax({
 
 var dogBreedListUrl = "https://dog.ceo/api/breeds/list/all";
 
-
 $.ajax({
     url: dogBreedListUrl,
     method: "GET"
 })
 .then(function(response) {
-    console.log(response);
-    var message = response.message;
-    console.log(message);
-    
-    var selectBox = $(".select-box");
-    
-
+    var message = response.message;  
+    var selectBox = $(".select-box");  
     for (var key in message) {
-        
         $('<option />', {value: key, text: key}).appendTo(selectBox);
-        
     }    
     
 });
-
-// var dogImageUrl = "https://dog.ceo/api/breed/" + "/images/random";
-
-// $(".select-box").change(function() {
-//     var selectedBreed = $(this).val();
-//     var dogImageUrl = "https://dog.ceo/api/breed/" + selectedBreed + "/images/random";
-//     $.ajax({ url: dogImageUrl , context: document.body, success: function(){
-//         console.log(selectedBreed);
-//     }});
-//   });
 
 
 
@@ -91,14 +73,8 @@ $("select").change(function(event){
         method: "GET"
     })
     .then(function(response) {
-        console.log(response);
         var image = response.message;
-        console.log(image);
-        
         var breedImage = $('.breed-image');
-        // //console.log("Article Image URL: " + response.articles[i].urlToImage);
-        // var articleImageURL = response.articles[i].urlToImage;
-        //breedImage.attr("class", "breed-image");
         breedImage.attr("src", image);
         breedImage.attr("style", "width: 400px");
         $(".dropDownImageSection").append(breedImage);        
